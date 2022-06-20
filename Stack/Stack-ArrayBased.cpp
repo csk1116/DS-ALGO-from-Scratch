e8df9f1cf51791f9796e2f88ff0077eb7ca7e536
csk1116 <u1116eric777@gmail.com>
2022-06-20 18:37:10 +0800

    add array based stack.

--- Stack/Stack-ArrayBased.cpp
+++ Stack/Stack-ArrayBased.cpp
@@ -4,18 +4,77 @@ using namespace std;
 class ArrayBasedStack
 {
 private:
-    int top;
+    int top; //index
     int capacity;
     int* stack;
     void doubleCapacity();
 public:
-    ArrayBasedStack(){}
-    void Push(int x);
+    ArrayBasedStack():top(-1), capacity(1) //capacity initial with one for testing
+    {
+        stack = new int[capacity];
+    }
+    void Push(int);
     void Pop();
     bool IsEmpty();
     int Top();
     int getSize();
 };
+
+void ArrayBasedStack::doubleCapacity()
+{
+    capacity *= 2;
+    int* newStack = new int[capacity];
+    for(int i = 0; i < capacity / 2; i ++)
+    {
+        newStack[i] = this->stack[i];
+    }    
+    delete[] stack;
+    stack = newStack;    
+}
+
+void ArrayBasedStack::Push(int input)
+{    
+    top ++;
+    if(top > capacity - 1)
+    {
+        doubleCapacity();
+    }
+    stack[top] = input;
+}
+
+void ArrayBasedStack::Pop()
+{
+    if(IsEmpty())
+    {
+        cout << "stack is empty" << endl;
+        return;
+    }
+
+    //revise the index of the top
+    stack[top] = 0; 
+    top --;
+}
+
+bool ArrayBasedStack::IsEmpty()
+{    
+    return top < 0 ? true : false;
+}
+
+int ArrayBasedStack::Top()
+{
+    if(IsEmpty())
+    {
+        cout << "stack is empty" << endl;
+        return -1;
+    }
+
+    return stack[top];
+}
+
+int ArrayBasedStack::getSize()
+{
+    return top + 1;
+}
 
 int main()
 {
